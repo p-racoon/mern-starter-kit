d08f22d518cc0e13d40b754bda6ca15dcd3aea6d
prasham ashesh <prasham.ashesh@infosys.com>
2019-12-05 18:52:18 +0530

    add iand intialize express nodemon server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const expressServer = express();
+
+const port = 5000;
+
+expressServer.get('/api/customers',(req,res)=>{
+    const customers = [
+        {id: 1, firstName: 'John', lastName:'Doe'},
+        {id: 2, firstName: 'Joel', lastName:'Egghead'},
+        {id: 3, firstName: 'Brad', lastName:'Traversy'},
+    ];
+    res.json(customers);
+})
+
+expressServer.listen(port, ()=>console.log(`Server started at port ${port}`));
